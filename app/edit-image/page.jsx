4fc361c064c2a675e1c5a4f2b866fc6a8a372cf1
Patryk Szymik <patryk.szymik01@wp.pl>
2023-09-07 21:09:29 +0200

    add handler for send image to server

--- app/edit-image/page.jsx
+++ app/edit-image/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Cropper } from 'react-advanced-cropper';
 import 'react-advanced-cropper/dist/style.css';
 
@@ -11,8 +11,27 @@ const EditImage = () => {
     'https://images.unsplash.com/photo-1599140849279-1014532882fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80'
   );
 
+  const cropperRef = useRef(null);
+
   const onChange = (cropper) => {
-    console.log(cropper.getCoordinates(), cropper.getCanvas());
+    const coordinates = cropper.getCoordinates();
+    const canvas = cropper.getCanvas();
+
+    return { coordinates, canvas };
+  };
+
+  const onSend = () => {
+    const canvas = cropperRef.current?.getCanvas();
+
+    if (canvas) {
+      const form = new FormData();
+      canvas.toBlob((blob) => {
+        if (blob) {
+          form.append('file', blob);
+          console.log(blob);
+        }
+      }, 'image/jpeg');
+    }
   };
 
   return (
@@ -25,6 +44,7 @@ const EditImage = () => {
         <Upload />
         <Cropper
           src={image}
+          ref={cropperRef}
           onChange={onChange}
           className="cropper"
           backgroundClassName="cropperImage"
@@ -32,9 +52,14 @@ const EditImage = () => {
             grid: true,
           }}
         />
-        <button type="button" className="black_btn">
-          <p className="text-base px-3 py-1">Crop Image</p>
-        </button>
+        <div className="flex flex-row gap-2">
+          <button type="button" className="black_btn" onClick={() => {}}>
+            <p className="text-base px-3 py-1">Crop Image</p>
+          </button>
+          <button type="button" className="black_btn" onClick={() => onSend()}>
+            <p className="text-base px-3 py-1">Send to Server</p>
+          </button>
+        </div>
       </div>
     </section>
   );
