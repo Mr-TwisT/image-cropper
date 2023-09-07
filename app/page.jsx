9c2c38348a058d4f06df85edeebd9627ff52d990
Patryk Szymik <patryk.szymik01@wp.pl>
2023-09-07 16:19:40 +0200

    my initial commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+        explicabo nesciunt quos pariatur quod rem asperiores, nulla fuga
+        accusantium perspiciatis fugit ut deleniti! Adipisci, accusamus.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
