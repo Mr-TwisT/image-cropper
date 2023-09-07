a1a47e8e64b04b9ae5dd2e27e908a7d7f79db973
Patryk Szymik <patryk.szymik01@wp.pl>
2023-09-07 19:53:57 +0200

    create upload button and fix some styles

---
+++ app/edit-image/page.jsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useState } from 'react';
+import { Cropper } from 'react-advanced-cropper';
+import 'react-advanced-cropper/dist/style.css';
+
+import Upload from '@components/Upload';
+
+const EditImage = () => {
+  const [image, setImage] = useState(
+    'https://images.unsplash.com/photo-1599140849279-1014532882fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80'
+  );
+
+  const onChange = (cropper) => {
+    console.log(cropper.getCoordinates(), cropper.getCanvas());
+  };
+
+  return (
+    <section className="w-full flex-center flex-col pb-14">
+      <h2 className="head_text text-center blue_gradient pb-10">
+        Edit Image Below
+      </h2>
+
+      <div className="h-screen flex flex-col gap-3 items-center mt-5">
+        <Upload />
+        <Cropper
+          src={image}
+          onChange={onChange}
+          className="cropper"
+          backgroundClassName="cropperImage"
+          stencilProps={{
+            grid: true,
+          }}
+        />
+        <button type="button" className="black_btn">
+          <p className="text-lg">Crop Image</p>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default EditImage;
---
+++ components/Upload.jsx
@@ -0,0 +1,55 @@
+import { useState, useRef, useEffect } from 'react';
+
+const Upload = () => {
+  const inputRef = useRef(null);
+
+  const [image, setImage] = useState(null);
+
+  const onUpload = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const onLoadImage = (e) => {
+    const { files } = e.target;
+
+    if (files && files[0]) {
+      const blob = URL.createObjectURL(files[0]);
+
+      setImage({
+        src: blob,
+        type: files[0].type,
+      });
+    }
+    e.target.value = '';
+  };
+
+  useEffect(() => {
+    return () => {
+      if (image && image.src) {
+        URL.revokeObjectURL(image.src);
+      }
+    };
+  }, [image]);
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="black_btn w-1/2 mx-auto"
+        onClick={onUpload}
+      >
+        <input
+          ref={inputRef}
+          type="file"
+          accept="image/*"
+          onChange={onLoadImage}
+          className="upload-file-input cursor-pointer ml-4"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Upload;
